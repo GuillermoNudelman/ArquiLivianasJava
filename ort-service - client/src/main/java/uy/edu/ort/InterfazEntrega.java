91b96175bae25f752b079e2409d6f9d575d3188b
GuillermoNudelman <nudelman.guillermo@gmail.com>
2016-10-26 21:58:48 -0200

    ultimo del dia

--- ort-service - client/src/main/java/uy/edu/ort/InterfazEntrega.java
+++ ort-service - client/src/main/java/uy/edu/ort/InterfazEntrega.java
@@ -5,6 +5,8 @@
  */
 package uy.edu.ort;
 
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -28,6 +30,38 @@ import uy.edu.ort.service.CamionetaService;
 import uy.edu.ort.service.ConvenioService;
 import uy.edu.ort.service.EntregaService;
 import uy.edu.ort.service.PaqueteService;
+
+import com.itextpdf.text.BaseColor;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import uy.edu.ort.model.Cliente;
+import uy.edu.ort.model.Paquete;
+import uy.edu.ort.service.PaqueteService;
+import uy.edu.ort.model.Camioneta;
+import uy.edu.ort.model.Entrega;
+import uy.edu.ort.utilities.ServicioBean;
+import uy.edu.ort.utilities.UsuarioService;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.CMYKColor;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 
 /**
  *
@@ -160,6 +194,12 @@ public final class InterfazEntrega {
                     ListarEntregas(entregaService.listEntregaPorMes(mes), paqueteService.listPaquetes());
                     break;
                 }
+                case 4: {
+                    System.out.println("Ingrese un mes (MM)");
+                    int mes = (int) esPositivo(in);
+                    generarPDFReporteMes(entregaService.listEntregaPorMes(mes), paqueteService.listPaquetes(), true);
+                    break;
+                }
                 case 5: {
                     System.out.println("Ingrese un mes (MM)");
                     int mes = (int) esPositivo(in);
@@ -170,6 +210,19 @@ public final class InterfazEntrega {
                     String codigoCamioneta = in.nextLine();
 
                     ListarEntregas(entregaService.listEntregaPorMesYCamioneta(mes, codigoCamioneta), paqueteService.listPaquetes());
+                    break;
+                }
+                case 6: {
+                    System.out.println("Ingrese un mes (MM)");
+                    int mes = (int) esPositivo(in);
+                    System.out.println("Seleccione una camioneta.");
+                    List<Camioneta> listadoCamionetas = camionetaService.listCamioneta();
+                    for (Camioneta camioneta : listadoCamionetas) {
+                        System.out.println(camioneta);
+                    }
+                    String codigoCamioneta = in.nextLine();
+
+                    generarPDFReporteMes(entregaService.listEntregaPorMesYCamioneta(mes, codigoCamioneta), paqueteService.listPaquetes(), false);
                     break;
                 }
                 default: {
@@ -214,5 +267,100 @@ public final class InterfazEntrega {
         Long kmActuales = camionetaAsociado.getKmsRecorridos();
         camionetaAsociado.setKmsRecorridos(kmActuales + distanciaARecorrer);
         camionetaService.editarCamioneta(camionetaAsociado);
+    }
+    
+    public static void generarPDFReporteMes(List<Entrega> listEntregas, List<Paquete> listPaquetes, boolean esReporteMesCamioneta) {
+        Document document = new Document();
+        try {
+            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("Reporte_Entregas_Mes.pdf"));
+            document.open();
+            Font redFont = FontFactory.getFont(FontFactory.COURIER, 12, Font.BOLD, new CMYKColor(0, 255, 0, 0));
+            Paragraph paragraphOne = new Paragraph();
+            if (esReporteMesCamioneta) {
+                paragraphOne = new Paragraph("Reporte de entregas para mes especifico", redFont);
+            } else {
+                paragraphOne = new Paragraph("Reporte de entregas para mes y camioneta especificos", redFont);
+            }
+
+            document.add(paragraphOne);
+
+            PdfPTable table = new PdfPTable(6); // 6 columns.
+            table.setWidthPercentage(100); //Width 100%
+            table.setSpacingBefore(10f); //Space before table
+            table.setSpacingAfter(10f); //Space after table
+
+            //Set Column widths
+            float[] columnWidths = {1f, 1f, 1f, 1f, 1f, 1f};
+            table.setWidths(columnWidths);
+
+            PdfPCell cell1 = new PdfPCell(new Paragraph("Código de Entrega"));
+            cell1.setBorderColor(BaseColor.GREEN);
+            cell1.setPaddingLeft(10);
+            cell1.setHorizontalAlignment(Element.ALIGN_CENTER);
+            cell1.setVerticalAlignment(Element.ALIGN_MIDDLE);
+
+            PdfPCell cell2 = new PdfPCell(new Paragraph("Fecha de Entrega"));
+            cell2.setBorderColor(BaseColor.GREEN);
+            cell2.setPaddingLeft(10);
+            cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
+            cell2.setVerticalAlignment(Element.ALIGN_MIDDLE);
+
+            PdfPCell cell3 = new PdfPCell(new Paragraph("Código de Camioneta"));
+            cell3.setBorderColor(BaseColor.GREEN);
+            cell3.setPaddingLeft(10);
+            cell3.setHorizontalAlignment(Element.ALIGN_CENTER);
+            cell3.setVerticalAlignment(Element.ALIGN_MIDDLE);
+
+            PdfPCell cell4 = new PdfPCell(new Paragraph("Peso total"));
+            cell4.setBorderColor(BaseColor.GREEN);
+            cell4.setPaddingLeft(10);
+            cell4.setHorizontalAlignment(Element.ALIGN_CENTER);
+            cell4.setVerticalAlignment(Element.ALIGN_MIDDLE);
+
+            PdfPCell cell5 = new PdfPCell(new Paragraph("Kilometros recorridos"));
+            cell5.setBorderColor(BaseColor.GREEN);
+            cell5.setPaddingLeft(10);
+            cell5.setHorizontalAlignment(Element.ALIGN_CENTER);
+            cell5.setVerticalAlignment(Element.ALIGN_MIDDLE);
+
+            PdfPCell cell6 = new PdfPCell(new Paragraph("Importe"));
+            cell6.setBorderColor(BaseColor.GREEN);
+            cell6.setPaddingLeft(10);
+            cell6.setHorizontalAlignment(Element.ALIGN_CENTER);
+            cell6.setVerticalAlignment(Element.ALIGN_MIDDLE);
+            table.addCell(cell1);
+            table.addCell(cell2);
+            table.addCell(cell3);
+            table.addCell(cell4);
+            table.addCell(cell5);
+            table.addCell(cell6);
+
+            for (Entrega entrega : listEntregas) {
+                cell1 = new PdfPCell(new Paragraph(entrega.getCodigo()));
+                cell2 = new PdfPCell(new Paragraph(entrega.getFechaEntrega().toString()));
+                cell3 = new PdfPCell(new Paragraph(entrega.getCamioneta().getCodigo()));
+                int pesoTotal = 0;
+                for (Paquete p : listPaquetes) {
+                    if (p.getEntrega().getCodigo().equals(entrega.getCodigo())) {
+                        pesoTotal += p.getPeso();
+                    }
+                }
+                cell4 = new PdfPCell(new Paragraph("" + pesoTotal));
+                cell5 = new PdfPCell(new Paragraph("" + entrega.getCamioneta().getKmsRecorridos()));
+                cell6 = new PdfPCell(new Paragraph("" + entrega.getImporteEntrega()));
+                table.addCell(cell1);
+                table.addCell(cell2);
+                table.addCell(cell3);
+                table.addCell(cell4);
+                table.addCell(cell5);
+                table.addCell(cell6);
+            }
+
+            document.add(table);
+            document.close();
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
